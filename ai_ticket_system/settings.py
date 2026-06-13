e93c3078053c59ebc806b5545fbe6646efd38079
Revathi K <revathiklucky@gmail.com>
2026-06-13 17:25:41 +0530

    Update settings.py

--- ai_ticket_system/settings.py
+++ ai_ticket_system/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ai_ticket_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'supportdb',
+        'USER': 'postgres',
+        'PASSWORD': 'revathi123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
